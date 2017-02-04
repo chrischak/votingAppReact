d1834d464d4ccef37164edeb2eb002e4b5a0f1b5
cchak <chrischakct@gmail.com>
2017-02-04 00:15:49 -0500

    added digital clock

---
+++ imports/client/DigitalClock.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('render-digital-clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+export class DigitalClock extends Component {
+
+  render(){
+    startTime();
+    return (
+    <div id="digital-clock">Hello World</div>
+    );
+  }
+}
