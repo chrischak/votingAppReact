07bfe957c1c19eae55d13f71e12259bc40524713
cchak <chrischakct@gmail.com>
2017-02-04 00:15:23 -0500

    added digital clock

--- client/main.js
+++ client/main.js
@@ -5,9 +5,11 @@ import { render } from 'react-dom';
 import App from '../imports/client/App';
 import { Clock } from '../imports/client/Clock';
 import { ConverterBox } from '../imports/client/CurrencyConverter';
+import { DigitalClock } from '../imports/client/DigitalClock'
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
   render(<Clock />, document.getElementById('render-clock'));
   render(<ConverterBox />, document.getElementById('render-currency-converter'));
+  render(<DigitalClock />, document.getElementById('render-digital-clock'));
 });
