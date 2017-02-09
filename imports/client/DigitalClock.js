0324282bb07006137f20f95138bfcf531e8c7cca
cchak <chrischakct@gmail.com>
2017-02-09 00:38:24 -0500

    added responsive text script

--- imports/client/DigitalClock.js
+++ imports/client/DigitalClock.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FitText from './fittext.js'
 
 function startTime() {
     var today = new Date();
@@ -9,6 +10,7 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('render-digital-clock').innerHTML =
     h + ":" + m + ":" + s;
+    fitText(document.getElementById('render-digital-clock'), 1);
     var t = setTimeout(startTime, 500);
 }
 
